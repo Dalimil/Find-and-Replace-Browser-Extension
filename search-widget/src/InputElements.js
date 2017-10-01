18b47100173bee0c4769bfe4c68cf0586562ec58
Dalimil Hajek <dalimilhajek@gmail.com>
2017-10-01 15:03:50 +0100

    Implement star-shaped checkbox

--- search-widget/src/InputElements.js
+++ search-widget/src/InputElements.js
@@ -1,6 +1,7 @@
 // Wrappers for HTML input elements - buttons, checkboxes, etc.
 
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
 
 const Button = ({title, onClick, disabled, small, style}) => (
   <div style={style ? style : {}}
@@ -13,17 +14,37 @@ const Button = ({title, onClick, disabled, small, style}) => (
 const Checkbox = ({name, checked, onChange, text: descr}) => {
   const id = "checkbox-" + name;
   return (
-    <div>
+    <div style={{ display: 'flex', alignItems: 'baseline' }}>
+      <label htmlFor={id} style={{ cursor: 'pointer', order: '1' }}> {descr}</label>
       <div className="checkbox">
         <input type="checkbox" name={name} id={id} checked={checked} onChange={onChange} />
         <label htmlFor={id}></label>
       </div>
-      <label htmlFor={id} style={{ cursor: 'pointer' }}> {descr}</label>
+    </div>
+  );
+};
+
+const Star = ({descrBefore, descrAfter, onClick, checked}) => {
+  return (
+    <div className="star-wrapper">
+      <span className="star-label"
+        onClick={onClick}> {checked ? descrAfter : descrBefore}</span>
+      <span className={"fa-stack fa-lg star-checkbox" + (checked ? " active" : "")}
+          onClick={onClick} >
+        <FontAwesome
+          name='star'
+          style={{ fontSize: '1.8em' }}
+          stack="1x" />
+        <FontAwesome
+          name='star-o'
+          stack="2x" />
+      </span>
     </div>
   );
 };
 
 export {
   Button,
-  Checkbox
+  Checkbox,
+  Star
 };
