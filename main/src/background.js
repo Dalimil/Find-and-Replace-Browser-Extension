9b4274a1417dab19e6d2d7198e7d23e84f77a642
Dalimil Hajek <dalimilhajek@gmail.com>
2017-10-02 23:08:12 +0100

    Fix browser inconsistencies for Firefox version

--- main/src/background.js
+++ main/src/background.js
@@ -1,18 +1,6 @@
 // Background extension page - Event page (only runs based on event listeners)
 // Console log messages will not be shown (only shown in special console)
 
-function setUpKeyboardCommands() {
-  const replaceAllManifestCommandName = 'replace-all-action';
-
-  // Keyboard shortcut handler
-  chrome.commands.onCommand.addListener(command => {
-    if (command == replaceAllManifestCommandName) {
-      // TODO: replace all - maybe turn this keyboard shortcut into a local-only one?
-      console.log('Replace all action triggered.');
-    }
-  });
-}
-
 function setUpContextMenu() {
   const contextMenuHandlingContentScriptFilepath = "src/page-content/context-menu-content-script.js";
   const contextMenuItemId = 'default_context_menu_item';
@@ -104,7 +92,6 @@ function setUpMessageConnections() {
 }
 
 // SET UP
-setUpKeyboardCommands();
 setUpContextMenu();
 setUpMessageConnections();
 
