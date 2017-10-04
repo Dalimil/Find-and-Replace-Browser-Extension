02e75109a4dcb063fb6d7281999e4e6d3642afd2
Dalimil Hajek <dalimilhajek@gmail.com>
2017-10-04 20:43:38 +0100

    Add Google Firebase Lambda functions

--- support-website/functions/index.js
+++ support-website/functions/index.js
@@ -1,8 +1,34 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// The Firebase Admin SDK to access the Firebase Realtime Database. 
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+// Inserts feedback message into our real time database
+function submitFeedback(endpointRef, feedbackMessage) {
+  if (!feedbackMessage) {
+    return Promise.resolve(400);
+  }
+  return admin.database().ref(endpointRef).push({
+    message: feedbackMessage,
+    timestamp: Date.now(),
+    timedate: new Date().toLocaleString()
+  })
+  .then(() => 200);
+}
+
+exports.submitUninstallFeedback = functions.https.onRequest((req, res) => {
+  const statusPromise = submitFeedback('/feedback/uninstall', JSON.parse(req.body).feedback);
+  statusPromise.then(status => {
+    res.status(status).end();
+  });
+});
+
+exports.submitGeneralFeedback = functions.https.onRequest((req, res) => {
+  const statusPromise = submitFeedback('/feedback/general', JSON.parse(req.body).feedback);
+  statusPromise.then(status => {
+    res.status(status).end();
+  });
+});
+
