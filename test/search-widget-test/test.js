986e2aef335edc3cdbae78450f881ad903b66b85
Dalimil Hajek <dalimilhajek@gmail.com>
2017-10-22 17:01:27 +0100

    Set up Mocha testing directory structure

--- test/search-widget-test/test.js
+++ test/search-widget-test/test.js
@@ -6,10 +6,6 @@ describe('Array', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal(-1, [1,2,3].indexOf(4));
     });
-
-    it('should return 0 when value is at the first index', function(){
-      assert.equal(0, [1,2,3].indexOf(1));
-    });
   });
 });
 
