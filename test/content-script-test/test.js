adb51496cf990533d156ad195e7fbe04718a7d49
Dalimil <dalimilhajek@gmail.com>
2018-01-14 18:53:47 +0000

    Set up content-script tests

--- test/content-script-test/test.js
+++ test/content-script-test/test.js
@@ -1,15 +1,111 @@
-// Test Demo
-// TODO - https://mochajs.org/
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
+// Testing suite for content-script.js
 
-    it('should return 0 when value is at the first index', function(){
-      assert.equal(0, [1,2,3].indexOf(1));
-    });
+function assert(condition, message) {
+  if (!condition) {
+      message = message || "Assertion failed";
+      if (typeof Error !== "undefined") {
+          throw new Error(message);
+      }
+      throw message;
+  } else {
+    console.log(".");
+  }
+}
+
+function assertEqual(a, b, message) {
+  assert(a == b, `${a} not equal ${b} ${message ? "- " + message : ""}`);
+}
+
+$(() => {
+  if (!window.FindAndReplaceExtension || !window.FindAndReplaceExtension.exposeApiDebugMode) {
+    console.error("Content script didn't initialize correctly.")
+  }
+  // Runtime messages don't work so add a timeout for API calls
+  const apiCall = (msg) => new Promise(resolve => {
+    window.FindAndReplaceExtension.exposeApiDebugMode(msg);
+    setTimeout(resolve, 500);
+  });
+
+  // Run Tests with the given API mock
+  runTests(apiCall).then(() => {
+    console.log("Done");
   });
 });
 
+// Tests
+async function runTests(apiCall) {
+  await apiCall({
+    action: 'updateSearch',
+    data: {
+      query: 'ok',
+      useRegex: false,
+      matchCase: false,
+      wholeWords: false,
+      limitToSelection: false,
+      includeOneLineFields: false,
+      replaceText: '_'
+    }
+  });
+  assertEqual($('.hwt-mark-highlight').length, 15);
+  assertEqual($('.hwt-highlight-current').length, 1);
+
+  await apiCall({
+    action: 'updateSearch',
+    data: {
+      query: 'ok',
+      useRegex: false,
+      matchCase: false,
+      wholeWords: false,
+      limitToSelection: false,
+      includeOneLineFields: true,
+      replaceText: '_'
+    }
+  });
+  assertEqual($('.hwt-mark-highlight').length, 20);
+  assertEqual($('.hwt-highlight-current').length, 1);
+
+  await apiCall({
+    action: 'updateSearch',
+    data: {
+      query: 'o.',
+      useRegex: false,
+      matchCase: false,
+      wholeWords: false,
+      limitToSelection: false,
+      includeOneLineFields: true,
+      replaceText: '_'
+    }
+  });
+  assertEqual($('.hwt-mark-highlight').length, 0);
+  assertEqual($('.hwt-highlight-current').length, 0);
+
+  await apiCall({
+    action: 'updateSearch',
+    data: {
+      query: 'o.',
+      useRegex: true,
+      matchCase: false,
+      wholeWords: false,
+      limitToSelection: false,
+      includeOneLineFields: true,
+      replaceText: '_'
+    }
+  });
+  assertEqual($('.hwt-mark-highlight').length, 36);
+  assertEqual($('.hwt-highlight-current').length, 1);
+
+  await apiCall({
+    action: 'updateSearch',
+    data: {
+      query: 'o.',
+      useRegex: true,
+      matchCase: true,
+      wholeWords: false,
+      limitToSelection: false,
+      includeOneLineFields: false,
+      replaceText: '_'
+    }
+  });
+  assertEqual($('.hwt-mark-highlight').length, 18);
+  assertEqual($('.hwt-highlight-current').length, 1);
+}
